20fbc3a6decd8f3bae88dda229cc9533ccdef7b2
David Vickery <davidvickery01@gmail.com>
2018-04-27 07:33:04 +0100

    Compiles now.

---
+++ src/layout_config_parser.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+namespace kensington
+{
+  class LayoutConfigParser
+  {
+    public:
+      LayoutConfigParser(const std::string& config) {}
+      size_t width() { return 0;}
+  };
+}
