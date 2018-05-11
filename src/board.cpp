4655bd5cb992ec71e36f4a53de728ddb1f57940d
David Vickery <davidvickery01@gmail.com>
2018-05-11 07:31:21 +0100

    Switched to clang.
    Also added clang-tidy into the make process so I get warned of style
    problems.

--- src/board.cpp
+++ src/board.cpp
@@ -1,5 +1,6 @@
 #include <board.h>
 
+#include <algorithm>
 #include <sstream>
 
 namespace kensington
@@ -7,7 +8,9 @@ namespace kensington
   bool Board::add_node(Node&& n)
   {
     if (n.location().first >= width_ || n.location().second >= height_)
+    {
       return false;
+    }
     if (std::find(std::begin(nodes_), std::end(nodes_), n) == nodes_.end())
     {
       nodes_.push_back(std::move(n));
@@ -17,15 +20,19 @@ namespace kensington
   }
   bool Board::add_edge(Edge&& e)
   {
-    return edges_.insert(std::move(e)).second;
+    return edges_.insert(e).second;
   }
   bool Board::occupy(Location loc, NodeStatus status)
   {
     auto node = std::find(std::begin(nodes_), std::end(nodes_), loc);
     if (node == nodes_.end())
+    {
       return false;
+    }
     if (node->status() != NodeStatus::EMPTY)
+    {
       return false;
+    }
     node->occupy(status);
     return true;
   }
@@ -44,18 +51,26 @@ namespace kensington
   {
     auto from_node = std::find(std::begin(nodes_), std::end(nodes_), from);
     if (from_node == nodes_.end())
+    {
       return false;
+    }
     auto to_node = std::find(std::begin(nodes_), std::end(nodes_), to);
     if (to_node == nodes_.end())
+    {
       return false;
+    }
     if (from_node->status() == NodeStatus::EMPTY
         || to_node->status() != NodeStatus::EMPTY)
+    {
       return false;
+    }
     if (edges_.find(Edge{*from_node,*to_node}) == edges_.end())
+    {
       return false;
+    }
 
     to_node->occupy(from_node->status());
     from_node->occupy(NodeStatus::EMPTY);
     return true;
   }
-}
+} // namespace kensington
